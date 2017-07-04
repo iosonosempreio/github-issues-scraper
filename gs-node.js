d022249de0a58377faceab8077cca875fafa05c4
iosonosempreio <tommaso.elli@gmail.com>
2017-07-04 23:26:06 +0200

    cleaning

--- gs-node.js
+++
@@ -1,5 +0,0 @@
-var gs = require('github-scraper'); // require the module
-var url = 'alanshaw' // a random username (of someone you should follow!)
-gs(url, function(err, data) {
-  console.log(data); // or what ever you want to do with the data
-})
